a61551d9d70fd1633542c0b9823c820c2c75f76e
basavanth <basavanthgoudapatil788@gmail.com>
2024-06-18 16:42:58 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Calculator {
+public double squareRoot(double num) {
+return Math.sqrt(num);
+}
+public double cubeRoot(double num) {
+return Math.cbrt(num);
+}
+}
+public class Main {
+public static void main(String[] args) {
+Calculator calculator = new Calculator();
+// Example usage
+double number = 64.0; // Example number
+double squareRoot = calculator.squareRoot(number);
+double cubeRoot = calculator.cubeRoot(number);
+System.out.println(&quot;Square root of &quot; + number + &quot; is: &quot; + squareRoot);
+System.out.println(&quot;Cube root of &quot; + number + &quot; is: &quot; + cubeRoot);
+}
+}
